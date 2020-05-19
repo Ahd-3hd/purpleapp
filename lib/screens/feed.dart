fe05ad3dec2f3e3f503a470ed788512583740fa1
Ahd <dafuq.ahd@gmail.com>
2020-05-20 02:02:13 +0300

    create ItemCard widget

--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -13,6 +13,7 @@ class _FeedState extends State<Feed> {
       child: Column(
         children: <Widget>[
           Expanded(
+            flex: 1,
             child: Stack(
               children: <Widget>[
                 Positioned(
@@ -44,7 +45,7 @@ class _FeedState extends State<Feed> {
                   top: 7,
                   child: RaisedButton(
                     padding: EdgeInsets.all(0),
-                    color: Colors.black.withOpacity(0),
+                    color: Colors.transparent,
                     elevation: 0,
                     onPressed: () {},
                     child: Icon(
@@ -56,9 +57,182 @@ class _FeedState extends State<Feed> {
                 ),
               ],
             ),
-          )
+          ),
+          Expanded(
+            flex: 5,
+            child: ListView(
+              children: <Widget>[
+                ItemCard(),
+                ItemCard(),
+                ItemCard(),
+                ItemCard(),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
+class ItemCard extends StatelessWidget {
+  const ItemCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/headerbg.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Title Here',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'TL 95.36',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            'Location',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 15, horizontal: 0),
+                    child: Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed turpis lectus, hendrerit a dignissim ac, pulvinar finibus nulla. Nullam ullamcorper quam nec pulvinar commodo.',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      RaisedButton(
+                        color: const Color(0xffB513A4),
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.insert_comment,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              'Whatsapp',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      RaisedButton(
+                        color: const Color(0xffB513A4),
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.phone,
+                              color: Colors.white,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              'Call',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      RaisedButton(
+                        color: Colors.transparent,
+                        elevation: 0,
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.rate_review,
+                              color: Colors.grey[800],
+                            ),
+                            SizedBox(width: 3),
+                            Text(
+                              'Reviews',
+                              style: TextStyle(
+                                color: Colors.grey[800],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// class ItemCard extends StatefulWidget {
+//   @override
+//   _ItemCardState createState() => _ItemCardState();
+// }
+
+// class _ItemCardState extends State<ItemCard> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
