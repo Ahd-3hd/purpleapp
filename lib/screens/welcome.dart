103b230f57143ca9bbf609436342a4c0a3a448ba
Ahd <dafuq.ahd@gmail.com>
2020-05-19 12:26:18 +0300

    create the welcome screen and add topright and bottom left background pngs]

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Welcome extends StatefulWidget {
   @override
@@ -8,6 +9,40 @@ class Welcome extends StatefulWidget {
 class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.white,
+      child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topRight,
+            child: Transform.translate(
+              offset: Offset(150.0, -100.0),
+              child: Image.asset('assets/Ellipse4.png'),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Transform.translate(
+              offset: Offset(170.0, -100.0),
+              child: Image.asset('assets/Ellipse6.png'),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Transform.translate(
+              offset: Offset(-150.0, 100.0),
+              child: Image.asset('assets/Ellipse5.png'),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Transform.translate(
+              offset: Offset(-170.0, 110.0),
+              child: Image.asset('assets/Ellipse7.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
