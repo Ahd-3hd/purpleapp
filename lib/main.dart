73ea6bf60465423eb7bc49781d2db95f17b4f61e
Ahd <dafuq.ahd@gmail.com>
2020-05-19 17:32:26 +0300

    change color and change initial route

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
     return MaterialApp(
-      initialRoute: '/loginorsignup',
+      initialRoute: '/',
       onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
