e8b4d3ff9665c0665a008931b64702cfb86134c1
Ahd <dafuq.ahd@gmail.com>
2020-05-20 15:55:37 +0300

    add floatingactionbutton to feed page

--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -10,6 +10,11 @@ class _FeedState extends State<Feed> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(
+            Icons.add_to_photos,
+          )),
       body: Container(
         color: Colors.white,
         child: Column(
