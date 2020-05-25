a9214aa942f5c2b3b716d59c6590dc9620a39aa8
Ahd <dafuq.ahd@gmail.com>
2020-05-25 22:02:27 +0300

    remove route generator, and create a wrapper that renders screens depending on the auth status provided by firebase

---
+++ lib/wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'models/user.dart';
+
+class Wrapper extends StatefulWidget {
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    if (user != null) {
+      return Text('logged in');
+    } else {
+      return Text('not logged');
+    }
+  }
+}
