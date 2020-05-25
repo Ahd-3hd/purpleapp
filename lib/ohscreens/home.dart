0214c4f8f3658b8ac02e305f5ffb8f8d4d5ef901
Ahd <dafuq.ahd@gmail.com>
2020-05-26 02:57:19 +0300

    add appbar

--- lib/ohscreens/home.dart
+++ lib/ohscreens/home.dart
@@ -9,6 +9,28 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.person,
+              size: 35,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        title: Text('Purple'),
+        elevation: 7,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[const Color(0xff8E2DE2), const Color(0xff4A00E0)],
+            ),
+          ),
+        ),
+      ),
       body: Container(),
     );
   }
