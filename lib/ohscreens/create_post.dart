3dd80e368985b9d31bfae830c86aa12ac778d3d9
Ahd <dafuq.ahd@gmail.com>
2020-05-28 23:05:45 +0300

    remove unused import

--- lib/ohscreens/create_post.dart
+++ lib/ohscreens/create_post.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart'; // For File Upload To Firestore
 import 'package:image_picker/image_picker.dart'; // For Image Picker
-import 'package:path/path.dart' as Path;
 import 'package:provider/provider.dart';
 import 'package:purple/ohmodels/user.dart';
 import 'package:purple/ohscreens/current_user_profile.dart';
